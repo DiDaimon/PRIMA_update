6556d7935fa6ff913c62c9d3cf2cb225f84784c4
DiDaimon <35574670+DiDaimon@users.noreply.github.com>
2025-11-13 17:03:48 +0300

    .gitignore - почищено от лишнего и отсортировано prima_update - добавлена проверка существования виртуального окружения, перехват ошибок, улучшение кода

--- prima_update.bat
+++ prima_update.bat
@@ -1,14 +1,55 @@
 @echo off
+:: Скрипт: prima_updater.bat
+:: Назначение: Запуск скрипта prima_updater.py
 
+:: Настройки
+set VENV_DIR=.venv
+set PYTHON=%VENV_DIR%\Scripts\Python.exe
+set PRIMA_DIR=D:\User\PRIMA_Updated\
+set PRIMA_EXE=%PRIMA_DIR%PRIMA.exe
+
+:: Перейти в директорию скрипта
 pushd %~dp0
 
-set TMP=.tmp
-set VENV_DIR=.venv
-set PYTHON="%VENV_DIR%\Scripts\Python.exe"
-%PYTHON% -c "" >%TMP%/stdout.txt 2>%TMP%/stderr.txt
+:: Смена кодировки на UTF-8 для корректного вывода сообщений на русском языке
+chcp 65001
+
+:: Проверка наличия виртуального окружения
+if not exist "%PYTHON%" (
+    echo [INFO] Виртуальное окружение не найдено. Создаю .venv...
+
+    :: Проверка доступности python
+    where python >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo [ERROR] Python не установлен или не добавлен в PATH.
+        echo Установите Python с https://www.python.org и перезапустите скрипт.
+        timeout /t 15 >nul
+        exit /b 1
+    )
+
+    :: Создание виртуального окружения
+    python -m venv %VENV_DIR%
+
+    if %errorlevel% neq 0 (
+        echo [ERROR] Не удалось создать виртуальное окружение.
+        timeout /t 15 >nul
+        exit /b 1
+    )
+
+    echo [SUCCESS] Виртуальное окружение создано.
+)
 %PYTHON% prima_update.py %*
 
-D:
-cd D:\User\PRIMA_Updated\
-start D:\User\PRIMA_Updated\PRIMA.exe
+:: Запуск PRIMA
+if exist %PRIMA_EXE% (
+    echo Запуск PRIMA.exe...
+    start %PRIMA_EXE%
+) else (
+    echo PRIMA.exe не найден в %PRIMA_DIR%
+    echo Проверьте путь к PRIMA.exe
+    timeout 5
+    exit /b 1
+    )
 timeout 5
+popd
+exit /b 0
